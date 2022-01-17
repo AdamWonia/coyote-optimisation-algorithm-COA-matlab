f364ff96e3f8dda65ed376678ff64cd24d540fa4
Adam Wonia <adam.wonia@gmail.com>
2022-01-17 19:26:22 +0100

    comments changed and made some refactorization

--- COA.m
+++ COA.m
@@ -1,115 +1,113 @@
-function [opt_dv, opt_OF] = COA(OF, lb, ub, max_iter, Ng, Nc)
-    %% Parametry algorytmu:
-    Ndv = size(lb, 2);  % ilość zmiennych decyzyjnych
-    dv_min = lb;      % wektor ograniczeń dolnych
-    dv_max = ub;      % wektor ograniczeń górnych
+function [opt_dv, opt_OF] = COA(OF, max_iter, Ng, Nc, dv_min, dv_max)
+    %% Parameters:
+    Ndv = size(dv_min, 2);  % number of decision variables
 
-    %% Inicjalizacja  algorytmu (krok 0):
-    % Liczba kojotów w populacji:
+    %% Algorithm initialization (step 0):
+    % Number of coyotes in population:
     Npop = Ng * Nc;
 
-    % Wartości funkcji celu na początku algorytmu:
+    % Initialization of objective function:
     FF_kg = zeros(Npop, 1);
 
-    %% Generowanie rozwiązań początkowych (krok 1):
-    % Dolne i górne ograniczenia:
+    %% Generation of initial solutions (step 1):
+    % Lower and upper bounds:
     Co_min = repmat(dv_min, Npop, 1);
     Co_max = repmat(dv_max, Npop, 1);
 
-    % Losowa wartość z przedziału [0,1]
-    gamma = rand(Npop, Ndv); % lepsze rozwiązania dla wektora rand niż jednej liczby
+    % Get random number in range of [0,1]
+    gamma = rand(Npop, Ndv);
 
-    % Utworzenie populacji początkowej kojotów:
+    % Creating an initial population of coyotes:
     Co_kg = Co_min + gamma .* (Co_max - Co_min);  
 
-    % Tworzenie grup z losowo wybieranych kojotów:
+    % Form groups from randomly selected coyotes:
     groups = reshape(randperm(Npop), Ng, []);
 
-    %% Obliczanie jakości populacji początkowej kojotów (krok 2):
-    % Współczynnik kary:
+    %% Calculation of quality of the initial coyote population (step 2):
+    % Penalty factor:
     pen_fact = 1000000; 
 
     for k = 1:Npop
-        % Sprawdzenie naruszenia ograniczeń:
+        % Verification of violation of restrictions:
         check_lb = Co_kg(k,:) < dv_min;
         check_ub = Co_kg(k,:) > dv_max;
         check_bound = check_lb + check_ub;
         penalty = sumsqr(sum(check_bound));
         PF = pen_fact * penalty;
 
-        % Obliczanie funkcji przystosowania z uwzględnieniem kary:
+        % Calculation of the objective function including penalty:
         FF_kg(k,1) = OF(Co_kg(k,:)) + PF;
     end
 
-    % Wyszukiwanie najmniejszej wartości funkcji celu:
+    % Searching for the smallest value of the objective function:
     [opt_OF, opt_idx] = min(FF_kg);
 
-    % Najlepsze rozwiązanie:
+    % Best solution:
     opt_dv = Co_kg(opt_idx,:);
 
-    %% Główna pętla algorytmu:
+    %% Main algorithm loop:
     iter = Npop;
 
-    % Kryterium stopu:
+    % Stopping criteria:
     while iter < max_iter 
 
-        % Dla każdego kojota p w grupie Ng: 
+        % For each p coyote in the Ng group: 
         for g = 1:Ng
 
-            % Wybieranie danej grupy kojotów i ich przystosowania:
+            % Selecting a particular group of coyotes and their values:
             C_new_pop = Co_kg(groups(g,:),:);
             FF_old = FF_kg(groups(g,:),:);
 
-            %% Wyznaczenie najlepszych lokalnych rozwiązań (krok 3):
+            %% Identification of best local solutions (step 3):
             Co_best = min(C_new_pop);
 
-            % Wyznaczenie rozwiązana środkowego (mediana):
+            % Finding the middle solution (median):
             Co_mid = median(C_new_pop); 
 
-            % Wybranie dwóch losowych rozwiązań w danej grupie:
+            % Selecting two random solutions per group:
             Co_new = zeros(Nc, Ndv);
 
             for c = 1:Nc
-                %% Wybranie dwóch losowych kojotów (bez powtórzeń):
+                %% Selecting two random coyotes (no repeats):
                 idx = randperm(Nc,2);
                 Co1 = C_new_pop(idx(1),:);
                 Co2 = C_new_pop(idx(2),:);
 
-                % Obliczanie nowych rozwiązań:
+                % Calculation of new solutions:
                 Co_new(c,:) = C_new_pop(c,:) + rand*(Co_best - Co1) + rand*(Co_mid  - Co2);
 
-                % Sprawdzenie naruszenia ograniczeń nowych rozwiązań:
+                % Checking violations of the limits of new solutions:
                 check_lb = Co_new(c,:) < dv_min;
                 check_ub = Co_new(c,:) > dv_max;
                 check_bound = check_lb + check_ub;
                 penalty = sumsqr(sum(check_bound));
                 PF = pen_fact * penalty;
 
-                %% Ocena przystosowania nowych rozwiązań (krok 4):
+                %% Calculation of OF for new solutions (step 4):
                 FF_new = OF(Co_new(c,:)) + PF;
 
-                %% Selekcja osobników (krok 5):
+                %% Selection of individuals (step 5):
                 if FF_new < FF_old(c, 1)
                     FF_old(c, 1) = FF_new;
                     C_new_pop(c,:) = Co_new(c,:);
                 end
 
-                % Aktualizacja iteracji:
+                % Update iteration::
                 iter = iter + 1;
             end
 
-            %% Utworzenie nowego rozwiązania w każdej grupie kojotów (krok 6):
-            % Wybieranie dwóch losowych osobników z nowej populacji (bez powtórzeń):
+            %% Creation of a new solution in each coyote group (step 6):
+            % Selection of two random individuals from a new population (no repeat):
             par_idx = randperm(Nc, 2);
             C_par1 = C_new_pop(par_idx(1),:);
             C_par2 = C_new_pop(par_idx(2),:);
 
-            % Tworzenie nowego rozwiązania (potomka):
+            % Creating a new solution:
             new_coy = zeros(1, Ndv);
             for x = 1:Ndv
-                % Losowa liczba z przedziału [0,1]:
+                % Get random number in range of [0,1]:
                 beta = rand;
-                % Wybieranie zmiennych decyzyjnych dla potomka:
+                % Selecting decision variables for new solution:
                 if beta < 1/Ndv
                     new_coy(1, x) = C_par1(1, x);
                 elseif beta >= 1/Ndv && beta < (0.5 + 1/Ndv)
@@ -121,42 +119,42 @@ function [opt_dv, opt_OF] = COA(OF, lb, ub, max_iter, Ng, Nc)
                 end
             end
 
-            % Obliczanie przystosowania nowego rozwiązania (zapewnione ograniczenia, więc bez kary):
+            % Calculation of the new solution:
             new_coy_cost = OF(new_coy);
 
-            %% Określenie najgorszego rozwiązania i zastąpienie go (krok 7):
+            %% Identifying the worst solution and replacing it (step 7):
             [Co_worst, worst_idx] = max(FF_old);
 
-            % Zastąpienie najgorszego rozwiązania:
+            % Replacing the worst solution:
             if new_coy_cost < Co_worst
                 FF_old(worst_idx,:) = new_coy_cost;
                 C_new_pop(worst_idx,:) = new_coy;
             end
 
-            % Aktualizacja grup kojotów:
+            % Update on coyote groups:
             Co_kg(groups(g,:),:) = C_new_pop;
             FF_kg(groups(g,:),:) = FF_old;
 
-            % Aktualizacja iteracji:
+            % Update iteration:
             iter = iter + 1;
         end
 
-        %% Wymiana rozwiązań pomiędzy grupami (krok 8):
+        %% Exchange of solutions between groups (step 8):
         if rand < (0.01/2) * Nc^2
-            % Losowanie dwóch grup i dwóch kojotów w danej grupie:
+            % Draw two groups and two coyotes per group:
             group_idx = randperm(Ng, 2);
             coy_idx = randperm(Nc, 2);
 
-            % Znalezienie kojotów do wymiany w grupach:
+            % Finding coyotes to exchange in groups:
             coy_ex1 = groups(group_idx(1), coy_idx(1));
             coy_ex2 = groups(group_idx(2), coy_idx(2));
 
-            % Wymiana rozwiązań w grupach:
+            % Exchange of solutions in groups:
             groups(group_idx(1), coy_idx(1)) = coy_ex2;
             groups(group_idx(2), coy_idx(2)) = coy_ex1;
         end
 
-        %% Określenie najlepszego rozwiązania (krok 9):
+        %% Determining the best solution (step 9):
         [opt_OF, opt_idx] = min(FF_kg);
         opt_dv = Co_kg(opt_idx,:);    
 
